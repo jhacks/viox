488884d3264ab8fc01a99e0f5fa0815758b1fafb
jhacks hack hersteller <jhackher@gmail.com>
2010-08-21 01:23:17 +0200

    license added

--- src/hcpl/hcpl.mk
+++ src/hcpl/hcpl.mk
@@ -1,3 +1,11 @@
+##
+##VIOX64
+##Virtual Bios Hypervisor 4 AMD64 Fam10+ 2 free & happy GPL3 hacking.
+##Author: jhacks hack hersteller jhackher@gmail.com
+##This program is released under the GNU GPL license v3 or lather, WHITHOUT ANY WARRANTY.
+##A copy of the license should be found in LICENSE.txt and/or COPYING.txt file.
+##In any case latest version is in http://www.gnu.org/licenses/gpl.html
+##
 #TODO
 
 
--- src/vx/main.cpp
+++ src/vx/main.cpp
@@ -6,6 +6,6 @@ This program is released under the GNU GPL license v3 or lather, WHITHOUT ANY WA
 A copy of the license should be found in LICENSE.txt and/or COPYING.txt file.
 In any case latest version is in http://www.gnu.org/licenses/gpl.html
 */
-#include hcpl.hpp
+#include main.hpp
 //TODO
 
